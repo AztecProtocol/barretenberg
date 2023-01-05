6dd45bad9a2c74afb8ce77f789dd9728ff3c2614
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-01-05 13:46:54 +0000

    Ported.

--- cpp/src/aztec/plonk/proof_system/types/plonk_proof.hpp
+++ cpp/src/aztec/plonk/proof_system/types/plonk_proof.hpp
@@ -2,6 +2,9 @@
 #include <cstdint>
 #include <vector>
 
+// Uncomment the following line to run the prover/verifier in a print-debug mode.
+// #define DEBUG_PROVER
+
 namespace waffle {
 
 struct plonk_proof {
