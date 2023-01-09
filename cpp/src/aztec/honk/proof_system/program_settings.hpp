4e6f3a5cdcfc570a5787404e598a42f30916634c
ledwards2225 <l.edwards.d@gmail.com>
2023-01-09 16:09:19 +0000

    simplify program_settings a bit

--- cpp/src/aztec/honk/proof_system/program_settings.hpp
+++ cpp/src/aztec/honk/proof_system/program_settings.hpp
@@ -11,13 +11,10 @@ namespace honk {
 class standard_verifier_settings : public waffle::standard_settings {
   public:
     typedef barretenberg::fr fr;
-    typedef barretenberg::g1 g1;
     typedef transcript::StandardTranscript Transcript;
 
     static constexpr size_t num_challenge_bytes = 32;
     static constexpr transcript::HashType hash_type = transcript::HashType::Keccak256;
-    static constexpr bool use_linearisation = true;
-    static constexpr bool idpolys = false;
 
     static constexpr size_t num_polys = StandardArithmetization::NUM_POLYNOMIALS;
 };
