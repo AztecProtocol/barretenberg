65f0494707faca114c6de156328b3a73f0496c8d
codygunton <codygunton@gmail.com>
2023-01-30 21:03:31 +0000

    Fix gcc issue.

--- cpp/src/aztec/honk/sumcheck/relations/relation.test.cpp
+++ cpp/src/aztec/honk/sumcheck/relations/relation.test.cpp
@@ -22,34 +22,34 @@ template <class FF> class SumcheckRelation : public testing::Test {
     template <size_t t> using UnivariateView = UnivariateView<FF, t>;
 
     // TODO(luke): may want to make this more flexible/genericzs
-    static std::array<Univariate<5>, bonk::StandardArithmetization::NUM_POLYNOMIALS> compute_mock_extended_edges()
+    static std::array<Univariate<6>, bonk::StandardArithmetization::NUM_POLYNOMIALS> compute_mock_extended_edges()
     {
         // TODO(Cody): build from Univariate<2>'s?
         // evaluation form, i.e. w_l(0) = 1, w_l(1) = 2,.. The poly is x+1.
-        auto w_l = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto w_r = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto w_o = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto z_perm = Univariate<5>({ 1, 2, 3, 4, 5 });
+        auto w_l = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto w_r = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto w_o = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto z_perm = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
         // Note: z_perm and z_perm_shift can be any linear poly for the sake of the tests but should not be be equal to
         // each other In order to avoid a trivial computation in the case of the grand_product_computation_relation.
         // Values here were chosen so that output univariates in tests are small positive numbers.
-        auto z_perm_shift = Univariate<5>({ 0, 1, 2, 3, 4 }); // this is not real shifted data
-        auto q_m = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto q_l = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto q_r = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto q_o = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto q_c = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto sigma_1 = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto sigma_2 = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto sigma_3 = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto id_1 = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto id_2 = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto id_3 = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto lagrange_first = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto lagrange_last = Univariate<5>({ 1, 2, 3, 4, 5 });
-        auto pow_zeta = Univariate<5>({ 1, 1, 1, 1, 1 });
+        auto z_perm_shift = Univariate<6>({ 0, 1, 2, 3, 4, 5 }); // this is not real shifted data
+        auto q_m = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto q_l = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto q_r = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto q_o = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto q_c = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto sigma_1 = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto sigma_2 = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto sigma_3 = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto id_1 = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto id_2 = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto id_3 = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto lagrange_first = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto lagrange_last = Univariate<6>({ 1, 2, 3, 4, 5, 6 });
+        auto pow_zeta = Univariate<6>({ 1, 1, 1, 1, 1, 1 });
 
-        std::array<Univariate<5>, bonk::StandardArithmetization::NUM_POLYNOMIALS> extended_edges = {
+        std::array<Univariate<6>, bonk::StandardArithmetization::NUM_POLYNOMIALS> extended_edges = {
             w_l,     w_r,  w_o,  z_perm, z_perm_shift,   q_m,           q_l,     q_r, q_o, q_c, sigma_1, sigma_2,
             sigma_3, id_1, id_2, id_3,   lagrange_first, lagrange_last, pow_zeta
         };
