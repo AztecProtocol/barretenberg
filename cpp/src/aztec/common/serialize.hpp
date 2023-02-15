c7652efa6cb5d655fe71b5b9d6c14b6b836a8a4f
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-02-15 06:59:12 +0000

    Port changes from `common`.
    Co-authored-by: iAmMichaelConnor <mike@aztecprotocol.com>

--- cpp/src/aztec/common/serialize.hpp
+++ cpp/src/aztec/common/serialize.hpp
@@ -311,6 +311,31 @@ template <typename B, typename T, typename U> inline void write(B& buf, std::map
     }
 }
 
+// Read std::optional<T>.
+template <typename B, typename T> inline void read(B& it, std::optional<T>& opt_value)
+{
+    bool is_nullopt;
+    read(it, is_nullopt);
+    if (is_nullopt) {
+        opt_value = std::nullopt;
+        return;
+    }
+    T value;
+    read(it, value);
+    opt_value = T(value);
+}
+
+// Write std::optional<T>.
+template <typename B, typename T> inline void write(B& buf, std::optional<T> const& opt_value)
+{
+    if (opt_value) {
+        write(buf, false); // is not nullopt
+        write(buf, *opt_value);
+        return;
+    }
+    write(buf, true); // is nullopt
+}
+
 } // namespace std
 
 // Helper functions that have return values.
