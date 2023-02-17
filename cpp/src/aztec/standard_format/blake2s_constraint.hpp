5036ed58aced38c8e29a46013c0d59bcd79a8850
kevaundray <kevtheappdev@gmail.com>
2023-02-17 22:08:36 +0000

    copy standard format

---
+++ cpp/src/aztec/standard_format/blake2s_constraint.hpp
@@ -0,0 +1,112 @@
+#include <common/serialize.hpp>
+#include <common/assert.hpp>
+#include <plonk/composer/composer_base.hpp>
+#include <plonk/composer/turbo_composer.hpp>
+#include <stdlib/hash/blake2s/blake2s.hpp>
+#include <crypto/blake2s/blake2s.hpp>
+#include <stdlib/types/types.hpp>
+#include <stdlib/primitives/packed_byte_array/packed_byte_array.hpp>
+
+using namespace plonk::stdlib::types;
+using namespace barretenberg;
+
+// Rounds a number to the nearest multiple of 8
+uint32_t blake_round_to_nearest_mul_8(uint32_t num_bits)
+{
+    uint32_t remainder = num_bits % 8;
+    if (remainder == 0)
+        return num_bits;
+
+    return num_bits + 8 - remainder;
+}
+// Rounds the number of bits to the nearest byte
+uint32_t blake_round_to_nearest_byte(uint32_t num_bits)
+{
+    return blake_round_to_nearest_mul_8(num_bits) / 8;
+}
+
+struct Blake2sInput {
+    uint32_t witness;
+    uint32_t num_bits;
+};
+
+struct Blake2sConstraint {
+    std::vector<Blake2sInput> inputs;
+    std::vector<uint32_t> result;
+};
+
+void create_blake2s_constraints(waffle::TurboComposer& composer, const Blake2sConstraint& constraint)
+{
+
+    // // Create byte array struct
+    byte_array_ct arr(&composer);
+
+    // // Get the witness assignment for each witness index
+    // // Write the witness assignment to the byte_array
+    for (const auto& witness_index_num_bits : constraint.inputs) {
+        auto witness_index = witness_index_num_bits.witness;
+        auto num_bits = witness_index_num_bits.num_bits;
+
+        // XXX: The implementation requires us to truncate the element to the nearest byte and not bit
+        auto num_bytes = blake_round_to_nearest_byte(num_bits);
+
+        field_ct element = field_ct::from_witness_index(&composer, witness_index);
+        byte_array_ct element_bytes(element, num_bytes);
+
+        arr.write(element_bytes);
+    }
+
+    byte_array_ct output_bytes = plonk::stdlib::blake2s<waffle::TurboComposer>(arr);
+
+    // Convert byte array to vector of field_t
+    auto bytes = output_bytes.bytes();
+
+    for (size_t i = 0; i < bytes.size(); ++i) {
+        composer.copy_from_to(bytes[i].normalize().witness_index, constraint.result[i]);
+    }
+}
+
+template <typename B> inline void read(B& buf, Blake2sInput& constraint)
+{
+    using serialize::read;
+    read(buf, constraint.witness);
+    read(buf, constraint.num_bits);
+}
+
+template <typename B> inline void write(B& buf, Blake2sInput const& constraint)
+{
+    using serialize::write;
+    write(buf, constraint.witness);
+    write(buf, constraint.num_bits);
+}
+
+inline bool operator==(Blake2sInput const& lhs, Blake2sInput const& rhs)
+{
+    // clang-format off
+    return
+        lhs.witness == rhs.witness &&
+        lhs.num_bits == rhs.num_bits;
+    // clang-format on
+}
+template <typename B> inline void read(B& buf, Blake2sConstraint& constraint)
+{
+    using serialize::read;
+    read(buf, constraint.inputs);
+    read(buf, constraint.result);
+}
+
+template <typename B> inline void write(B& buf, Blake2sConstraint const& constraint)
+{
+    using serialize::write;
+    write(buf, constraint.inputs);
+    write(buf, constraint.result);
+}
+
+inline bool operator==(Blake2sConstraint const& lhs, Blake2sConstraint const& rhs)
+{
+    // clang-format off
+    return
+        lhs.inputs == rhs.inputs &&
+        lhs.result == rhs.result;
+    // clang-format on
+}
---
+++ cpp/src/aztec/standard_format/pedersen.hpp
@@ -0,0 +1,64 @@
+#include <common/serialize.hpp>
+#include <common/assert.hpp>
+#include <plonk/composer/composer_base.hpp>
+#include <plonk/composer/turbo_composer.hpp>
+#include <stdlib/hash/pedersen/pedersen.hpp>
+#include <stdlib/types/types.hpp>
+#include <stdlib/primitives/packed_byte_array/packed_byte_array.hpp>
+#include <stdlib/primitives/byte_array/byte_array.hpp>
+
+using namespace plonk::stdlib::types;
+
+typedef plonk::stdlib::field_t<waffle::TurboComposer> field_t;
+typedef plonk::stdlib::byte_array<waffle::TurboComposer> byte_array;
+
+using namespace barretenberg;
+using namespace plonk;
+
+// P = xG + bH
+struct PedersenConstraint {
+    std::vector<uint32_t> scalars;
+    uint32_t result_x;
+    uint32_t result_y;
+};
+
+void create_pedersen_constraint(waffle::TurboComposer& composer, const PedersenConstraint& input)
+{
+    std::vector<field_t> scalars;
+
+    for (const auto& scalar : input.scalars) {
+        // convert input indices to field_t
+        field_t scalar_as_field = field_t::from_witness_index(&composer, scalar);
+        scalars.push_back(scalar_as_field);
+    }
+    auto point = pedersen::commit(scalars);
+
+    composer.copy_from_to(point.x.witness_index, input.result_x);
+    composer.copy_from_to(point.y.witness_index, input.result_y);
+}
+
+template <typename B> inline void read(B& buf, PedersenConstraint& constraint)
+{
+    using serialize::read;
+    read(buf, constraint.scalars);
+    read(buf, constraint.result_x);
+    read(buf, constraint.result_y);
+}
+
+template <typename B> inline void write(B& buf, PedersenConstraint const& constraint)
+{
+    using serialize::write;
+    write(buf, constraint.scalars);
+    read(buf, constraint.result_x);
+    read(buf, constraint.result_y);
+}
+
+inline bool operator==(PedersenConstraint const& lhs, PedersenConstraint const& rhs)
+{
+    // clang-format off
+    return
+        lhs.scalars == rhs.scalars &&
+        lhs.result_x == rhs.result_x &&
+        lhs.result_y == rhs.result_y;
+    // clang-format on
+}
