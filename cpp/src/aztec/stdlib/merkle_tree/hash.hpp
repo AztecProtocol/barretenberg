e8281300ed033d2d893e753e5510bed7286a699d
Suyash Bagad <suyash@aztecprotocol.com>
2023-02-22 22:28:01 +0530

    Switch to Turbo Temporarily. (#174)
    * Switch to turbo.
    
    * Correct merkle tree hash type (make it composer-agnostic).
    
    * Change js vk.
    
    * change js size.

--- cpp/src/aztec/stdlib/merkle_tree/hash.hpp
+++ cpp/src/aztec/stdlib/merkle_tree/hash.hpp
@@ -15,7 +15,11 @@ namespace merkle_tree {
 
 inline barretenberg::fr compress_native(barretenberg::fr const& lhs, barretenberg::fr const& rhs)
 {
-    return crypto::pedersen_hash::lookup::hash_multiple({ lhs, rhs });
+    if (waffle::SYSTEM_COMPOSER == waffle::PLOOKUP) {
+        return crypto::pedersen_hash::lookup::hash_multiple({ lhs, rhs });
+    } else {
+        return crypto::pedersen_hash::hash_multiple({ lhs, rhs });
+    }
 }
 
 } // namespace merkle_tree
