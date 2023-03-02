7b8c07a4a7d5c7142006f244475b4525b2a88fb6
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-03-02 13:36:25 +0000

    Fix.

--- cpp/src/aztec/stdlib/types/convert.hpp
+++ cpp/src/aztec/stdlib/types/convert.hpp
@@ -59,7 +59,7 @@ typename CT<Composer>::bn254_point to_ct(Composer& composer, typename NT::bn254_
 template <typename Composer>
 typename CT<Composer>::ecdsa_signature to_ct(Composer& composer, typename NT::ecdsa_signature const& e)
 {
-    return CT<Composer>::ecdsa_signature::from_witness<Composer>(&composer, e);
+    return CT<Composer>::ecdsa_signature::template from_witness<Composer>(&composer, e);
 };
 
 template <typename Composer>
