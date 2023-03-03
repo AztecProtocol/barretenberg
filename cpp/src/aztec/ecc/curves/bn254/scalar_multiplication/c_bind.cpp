b581489c6c2e85c4f77579a093e6a3efe34bc617
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-03 12:35:10 -0700

    fix!: Change new_pippenger points argument to uint8_t

--- cpp/src/aztec/ecc/curves/bn254/scalar_multiplication/c_bind.cpp
+++ cpp/src/aztec/ecc/curves/bn254/scalar_multiplication/c_bind.cpp
@@ -18,7 +18,7 @@ WASM_EXPORT void bbfree(void* ptr)
     aligned_free(ptr);
 }
 
-WASM_EXPORT void* new_pippenger(g1::affine_element* points, size_t num_points)
+WASM_EXPORT void* new_pippenger(uint8_t* points, size_t num_points)
 {
     auto ptr = new scalar_multiplication::Pippenger(points, num_points);
     return ptr;
--- cpp/src/aztec/ecc/curves/bn254/scalar_multiplication/c_bind.hpp
+++ cpp/src/aztec/ecc/curves/bn254/scalar_multiplication/c_bind.hpp
@@ -9,7 +9,7 @@ WASM_EXPORT void* bbmalloc(size_t size);
 
 WASM_EXPORT void bbfree(void* ptr);
 
-WASM_EXPORT void* new_pippenger(barretenberg::g1::affine_element* points, size_t num_points);
+WASM_EXPORT void* new_pippenger(uint8_t* points, size_t num_points);
 
 WASM_EXPORT void delete_pippenger(void* pippenger);
 
