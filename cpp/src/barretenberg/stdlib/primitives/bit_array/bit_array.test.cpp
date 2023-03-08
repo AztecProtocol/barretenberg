3a4b0d82aa141246b977210d16ded9448dea712e
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-08 14:16:48 -0600

    Remove comment so to avoid incorrect include

--- cpp/src/barretenberg/stdlib/primitives/bit_array/bit_array.test.cpp
+++ cpp/src/barretenberg/stdlib/primitives/bit_array/bit_array.test.cpp
@@ -2,7 +2,6 @@
 #include <gtest/gtest.h>
 #include "barretenberg/numeric/random/engine.hpp"
 #include "barretenberg/honk/composer/standard_honk_composer.hpp"
-// #include <plonk/composer/standard_composer.hpp>
 
 namespace test_stdlib_bit_array {
 
