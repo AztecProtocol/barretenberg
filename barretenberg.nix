ab60cb9305450bcc76b8c280bc481163bb59725f
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-09 15:35:14 -0600

    Try to use stdenv on x86 linux

--- barretenberg.nix
+++ barretenberg.nix
@@ -3,11 +3,12 @@ let
   targetPlatform = stdenv.targetPlatform;
   # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 but it seems to break the wasm build
   # so we check to see if the targetPlatform is wasm and just the llvmPackages.stdenv
-  buildEnv =
-    if (stdenv.targetPlatform.isWasm) then
-      llvmPackages.stdenv
-    else
-      overrideCC llvmPackages.stdenv (llvmPackages.clang.override { gccForLibs = gcc11.cc; });
+  buildEnv = llvmPackages.stdenv;
+  # buildEnv =
+  #   if (stdenv.targetPlatform.isWasm) then
+  #     llvmPackages.stdenv
+  #   else
+  #     overrideCC llvmPackages.stdenv (llvmPackages.clang.override { gccForLibs = gcc11.cc; });
   optionals = lib.lists.optionals;
   toolchain_file = ./cpp/cmake/toolchains/${targetPlatform.system}.cmake;
 in
