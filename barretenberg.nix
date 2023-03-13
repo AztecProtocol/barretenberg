c0102df77e852d102f01cb88bdf420a8aed39d85
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-13 07:29:13 -0700

    Need to be explicit about gcc11 for aarch64 linux

--- barretenberg.nix
+++ barretenberg.nix
@@ -1,11 +1,11 @@
-{ overrideCC, stdenv, llvmPackages, cmake, ninja, lib, callPackage, binaryen, gcc }:
+{ overrideCC, stdenv, llvmPackages, cmake, ninja, lib, callPackage, binaryen, gcc11 }:
 let
   targetPlatform = stdenv.targetPlatform;
   buildEnv =
     if (stdenv.targetPlatform.isGnu && stdenv.targetPlatform.isAarch64) then
     # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
     # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++
-      overrideCC llvmPackages.stdenv (llvmPackages.clang.override { gccForLibs = gcc.cc; })
+      overrideCC llvmPackages.stdenv (llvmPackages.clang.override { gccForLibs = gcc11.cc; })
     else
       llvmPackages.stdenv;
   # buildEnv =
