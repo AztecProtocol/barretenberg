4fd5be269715ef8619521cf9a6fa12a795fa3fe5
Rumata888 <isennovskiy@gmail.com>
2023-03-23 23:08:52 +0000

    Turbo Circuit Constructor working

--- cpp/src/barretenberg/honk/composer/standard_plonk_composer.hpp
+++ cpp/src/barretenberg/honk/composer/standard_plonk_composer.hpp
@@ -29,7 +29,7 @@ class StandardPlonkComposer {
 
     // Leaving it in for now just in case
     bool contains_recursive_proof = false;
-    static constexpr size_t program_width = STANDARD_HONK_WIDTH;
+    static constexpr size_t program_width = STANDARD_BONK_WIDTH;
 
     /**Standard methods*/
 
