c25291d771adaa58189248f1582921c5eadaf508
Suyash Bagad <suyash@aztecprotocol.com>
2023-03-27 13:02:28 +0100

    proof serialisation. (#269)

--- cpp/src/barretenberg/plonk/proof_system/types/proof.hpp
+++ cpp/src/barretenberg/plonk/proof_system/types/proof.hpp
@@ -1,11 +1,34 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include "barretenberg/common/serialize.hpp"
 
 namespace plonk {
 
 struct proof {
     std::vector<uint8_t> proof_data;
 };
+
+inline void read(uint8_t const*& it, proof& data)
+{
+    using serialize::read;
+    read(it, data.proof_data);
+};
+
+template <typename B> inline void write(B& buf, proof& data)
+{
+    using serialize::write;
+    write(buf, data.proof_data);
+}
+
+inline std::ostream& operator<<(std::ostream& os, proof const& data)
+{
+    os << "[\n ";
+    for (size_t i = 0; i < data.proof_data.size(); ++i) {
+        os << data.proof_data[i] << " ";
+    }
+    os << "]\n";
+    return os;
+}
 
 } // namespace plonk
