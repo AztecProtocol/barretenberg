7ceb93c50c0322244172c4f9cc66c41115aa797a
Santiago Palladino <santiago@aztecprotocol.com>
2023-03-28 11:10:35 +0100

    Fix serialization native agg state (#283)
    * Fix native agg state serialization and proof output
    
    Fixes serialization tests on aztec circuits.
    
    * Fix duplicated operator error
    
    * Remove unused import
    
    * Add missing includes

--- cpp/src/barretenberg/plonk/proof_system/types/proof.hpp
+++ cpp/src/barretenberg/plonk/proof_system/types/proof.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include <ostream>
+#include <iomanip>
 #include "barretenberg/common/serialize.hpp"
 
 namespace plonk {
@@ -23,11 +25,17 @@ template <typename B> inline void write(B& buf, proof const& data)
 
 inline std::ostream& operator<<(std::ostream& os, proof const& data)
 {
-    os << "[\n ";
-    for (size_t i = 0; i < data.proof_data.size(); ++i) {
-        os << data.proof_data[i] << " ";
+    // REFACTOR: This is copied from barretenberg/common/streams.hpp,
+    // which means we could just cout proof_data directly, but that breaks the build in the CI with 
+    // a redefined operator<< error in barretenberg/stdlib/hash/keccak/keccak.test.cpp,
+    // which is something we really don't want to deal with right now.
+    std::ios_base::fmtflags f(os.flags());
+    os << "[" << std::hex << std::setfill('0');
+    for (auto byte : data.proof_data) {
+        os << ' ' << std::setw(2) << +(unsigned char)byte;
     }
-    os << "]\n";
+    os << " ]";
+    os.flags(f);
     return os;
 }
 
