592514e533d7aaf20f5631b3a2dfa38ea61c9c9c
zac-williamson <blorktronics@gmail.com>
2023-03-29 17:02:19 +0000

    updated acir_format to use new stdlib logic class
    Updated ReadMe to include wasm example that supports gtest filtering

--- cpp/src/barretenberg/dsl/acir_format/logic_constraint.cpp
+++ cpp/src/barretenberg/dsl/acir_format/logic_constraint.cpp
@@ -1,5 +1,7 @@
 #include "logic_constraint.hpp"
 
+#include "barretenberg/stdlib/primitives/logic/logic.hpp"
+
 namespace acir_format {
 
 // TODO(maxim): This doesn't work correctly
@@ -10,42 +12,13 @@ void create_logic_gate(Composer& composer,
                        const size_t num_bits,
                        const bool is_xor_gate)
 {
-    const size_t num_chunks = (num_bits / 32) + ((num_bits % 32 == 0) ? 0 : 1);
-    uint256_t left = composer.get_variable(a);
-    uint256_t right = composer.get_variable(b);
 
-    field_ct res(&composer);
-    for (size_t i = 0; i < num_chunks; ++i) {
-        uint256_t left_chunk = left & ((uint256_t(1) << 32) - 1);
-        uint256_t right_chunk = right & ((uint256_t(1) << 32) - 1);
+    field_ct left = field_ct::from_witness_index(&composer, a);
+    field_ct right = field_ct::from_witness_index(&composer, b);
 
-        const field_ct a(&composer, left_chunk);
-        const field_ct b(&composer, right_chunk);
-
-        field_ct result_chunk = 0;
-        if (is_xor_gate) {
-            result_chunk = plookup_read_ct::read_from_2_to_1_table(plookup::MultiTableId::UINT32_AND, a, b);
-        } else {
-            result_chunk = plookup_read_ct::read_from_2_to_1_table(plookup::MultiTableId::UINT32_AND, a, b);
-        }
-
-        uint256_t scaling_factor = uint256_t(1) << (32 * i);
-        res += result_chunk * scaling_factor;
-
-        if (i == num_chunks - 1) {
-            const size_t final_num_bits = num_bits - (i * 32);
-            if (final_num_bits != 32) {
-                composer.create_range_constraint(a.witness_index, final_num_bits, "bad range on a");
-                composer.create_range_constraint(b.witness_index, final_num_bits, "bad range on b");
-            }
-        }
-
-        left = left >> 32;
-        right = right >> 32;
-    }
-
-    auto our_res = field_ct::from_witness_index(&composer, result);
-    (our_res + res);
+    field_ct res = stdlib::logic<Composer>::create_logic_constraint(left, right, num_bits, is_xor_gate);
+    field_ct our_res = field_ct::from_witness_index(&composer, result);
+    res.assert_equal(our_res);
 }
 
 } // namespace acir_format
