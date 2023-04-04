9f71673f36ed36da1be8f448e67042b0b114ae80
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-04-04 01:41:28 +0000

    inc num_generators_per_hash_index to 128.

--- cpp/src/barretenberg/crypto/generators/generator_data.cpp
+++ cpp/src/barretenberg/crypto/generators/generator_data.cpp
@@ -13,7 +13,7 @@ constexpr size_t num_hash_indices = 32;
 #else
 constexpr size_t num_default_generators = 2048;
 constexpr size_t num_hash_indices = 32;
-constexpr size_t num_generators_per_hash_index = 64;
+constexpr size_t num_generators_per_hash_index = 128;
 #endif
 
 constexpr size_t hash_indices_generator_offset = 2048;
