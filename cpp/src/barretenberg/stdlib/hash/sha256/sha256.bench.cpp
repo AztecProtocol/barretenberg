b35430b315f93c5c70c1f12b1911632488189208
codygunton <codygunton@gmail.com>
2023-04-14 16:58:53 +0000

    Standardize aliases

--- cpp/src/barretenberg/stdlib/hash/sha256/sha256.bench.cpp
+++ cpp/src/barretenberg/stdlib/hash/sha256/sha256.bench.cpp
@@ -6,11 +6,10 @@
 #include "barretenberg/stdlib/primitives/packed_byte_array/packed_byte_array.hpp"
 
 using namespace benchmark;
-using namespace proof_system::plonk;
 
-using LocalComposer = UltraComposer;
-using LocalProver = UltraProver;
-using LocalVerifier = UltraVerifier;
+using Composer = proof_system::plonk::UltraComposer;
+using Prover = proof_system::plonk::UltraProver;
+using Verifier = proof_system::plonk::UltraVerifier;
 
 constexpr size_t NUM_HASHES = 8;
 constexpr size_t BYTES_PER_CHUNK = 512;
@@ -22,27 +21,27 @@ char get_random_char()
     return static_cast<char>(barretenberg::fr::random_element().data[0] % 8);
 }
 
-void generate_test_plonk_circuit(LocalComposer& composer, size_t num_bytes)
+void generate_test_plonk_circuit(Composer& composer, size_t num_bytes)
 {
     std::string in;
     in.resize(num_bytes);
     for (size_t i = 0; i < num_bytes; ++i) {
         in[i] = get_random_char();
     }
-    stdlib::packed_byte_array<LocalComposer> input(&composer, in);
-    stdlib::sha256<LocalComposer>(input);
+    proof_system::plonk::stdlib::packed_byte_array<Composer> input(&composer, in);
+    proof_system::plonk::stdlib::sha256<Composer>(input);
 }
 
-LocalComposer composers[NUM_HASHES];
-LocalProver provers[NUM_HASHES];
-LocalVerifier verifiers[NUM_HASHES];
+Composer composers[NUM_HASHES];
+Prover provers[NUM_HASHES];
+Verifier verifiers[NUM_HASHES];
 plonk::proof proofs[NUM_HASHES];
 
 void construct_witnesses_bench(State& state) noexcept
 {
     for (auto _ : state) {
         size_t idx = (static_cast<size_t>((state.range(0))) - START_BYTES) / BYTES_PER_CHUNK;
-        composers[idx] = LocalComposer();
+        composers[idx] = Composer();
         generate_test_plonk_circuit(composers[idx], static_cast<size_t>(state.range(0)));
     }
 }
