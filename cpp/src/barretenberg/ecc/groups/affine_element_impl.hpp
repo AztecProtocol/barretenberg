fb3212570f2842a9265ff92dd5c996840225fd4c
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-04-16 14:25:16 +0000

    Add `random_element` to affine element.

--- cpp/src/barretenberg/ecc/groups/affine_element_impl.hpp
+++ cpp/src/barretenberg/ecc/groups/affine_element_impl.hpp
@@ -202,5 +202,26 @@ affine_element<Fq, Fr, T> affine_element<Fq, Fr, T>::hash_to_curve(const uint64_
 
     return affine_element<Fq, Fr, T>(x_out, y_out_);
 }
+
+template <typename Fq, typename Fr, typename T>
+affine_element<Fq, Fr, T> affine_element<Fq, Fr, T>::random_element(numeric::random::Engine* engine) noexcept
+{
+    bool found_one = false;
+    Fq yy;
+    Fq x;
+    Fq y;
+    while (!found_one) {
+        x = Fq::random_element(engine);
+        yy = x.sqr() * x + T::b;
+        if constexpr (T::has_a) {
+            yy += (x * T::a);
+        }
+        auto [found_root, y1] = yy.sqrt();
+        y = y1;
+        found_one = found_root;
+    }
+    return affine_element<Fq, Fr, T>(x, y);
+}
+
 } // namespace group_elements
 } // namespace barretenberg
