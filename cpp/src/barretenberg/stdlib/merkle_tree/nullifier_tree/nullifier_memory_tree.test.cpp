48783c2d7db1291b65890e24c05fc4d52cb93c00
cheethas <urmasurda@gmail.com>
2023-04-17 15:02:08 +0000

    fix: add append zero behaviour and test

--- cpp/src/barretenberg/stdlib/merkle_tree/nullifier_tree/nullifier_memory_tree.test.cpp
+++ cpp/src/barretenberg/stdlib/merkle_tree/nullifier_tree/nullifier_memory_tree.test.cpp
@@ -161,6 +161,171 @@ TEST(crypto_nullifier_tree, test_nullifier_memory)
     EXPECT_EQ(tree.get_hash_path(7), expected);
 }
 
+TEST(crypto_nullifier_tree, test_nullifier_memory_appending_zero)
+{
+    // Create a depth-3 indexed merkle tree
+    constexpr size_t depth = 3;
+    NullifierMemoryTree tree(depth);
+
+    /**
+     * Intial state:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       0       0       0        0       0       0       0
+     *  nextIdx   0       0       0       0        0       0       0       0
+     *  nextVal   0       0       0       0        0       0       0       0
+     */
+    nullifier_leaf zero_leaf = { 0, 0, 0 };
+    EXPECT_EQ(tree.get_leaves().size(), 1);
+    EXPECT_EQ(tree.get_leaves()[0], zero_leaf);
+
+    /**
+     * Add new value 30:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       30      0       0        0       0       0       0
+     *  nextIdx   1       0       0       0        0       0       0       0
+     *  nextVal   30      0       0       0        0       0       0       0
+     */
+    tree.update_element(30);
+    EXPECT_EQ(tree.get_leaves().size(), 2);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 1, 30 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 0, 0 }).hash());
+
+    /**
+     * Add new value 10:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       30      10      0        0       0       0       0
+     *  nextIdx   2       0       1       0        0       0       0       0
+     *  nextVal   10      0       30      0        0       0       0       0
+     */
+    tree.update_element(10);
+    EXPECT_EQ(tree.get_leaves().size(), 3);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 2, 10 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 0, 0 }).hash());
+    EXPECT_EQ(tree.get_leaves()[2].hash(), NullifierLeaf({ 10, 1, 30 }).hash());
+
+    /**
+     * Add new value 20:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       30      10      20       0       0       0       0
+     *  nextIdx   2       0       3       1        0       0       0       0
+     *  nextVal   10      0       20      30       0       0       0       0
+     */
+    tree.update_element(20);
+    EXPECT_EQ(tree.get_leaves().size(), 4);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 2, 10 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 0, 0 }).hash());
+    EXPECT_EQ(tree.get_leaves()[2].hash(), NullifierLeaf({ 10, 3, 20 }).hash());
+    EXPECT_EQ(tree.get_leaves()[3].hash(), NullifierLeaf({ 20, 1, 30 }).hash());
+
+    // Adding the same value must not affect anything
+    tree.update_element(20);
+    EXPECT_EQ(tree.get_leaves().size(), 4);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 2, 10 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 0, 0 }).hash());
+    EXPECT_EQ(tree.get_leaves()[2].hash(), NullifierLeaf({ 10, 3, 20 }).hash());
+    EXPECT_EQ(tree.get_leaves()[3].hash(), NullifierLeaf({ 20, 1, 30 }).hash());
+
+    /**
+     * Add new value 0:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       30      10      20       0       0       0       0
+     *  nextIdx   2       0       3       1        0       0       0       0
+     *  nextVal   10      0       20      30       0       0       0       0
+     */
+    tree.update_element(0);
+    EXPECT_EQ(tree.get_leaves().size(), 5);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 2, 10 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 0, 0 }).hash());
+    EXPECT_EQ(tree.get_leaves()[2].hash(), NullifierLeaf({ 10, 3, 20 }).hash());
+    EXPECT_EQ(tree.get_leaves()[3].hash(), NullifierLeaf({ 20, 1, 30 }).hash());
+    EXPECT_EQ(tree.get_leaves()[4].hash(), NullifierLeaf::zero().hash());
+
+    /*
+     * Add new value 0:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       30      10      20       0       0       0       0
+     *  nextIdx   2       0       3       1        0       0       0       0
+     *  nextVal   10      0       20      30       0       0       0       0
+     */
+    tree.update_element(0);
+    EXPECT_EQ(tree.get_leaves().size(), 6);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 2, 10 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 0, 0 }).hash());
+    EXPECT_EQ(tree.get_leaves()[2].hash(), NullifierLeaf({ 10, 3, 20 }).hash());
+    EXPECT_EQ(tree.get_leaves()[3].hash(), NullifierLeaf({ 20, 1, 30 }).hash());
+    EXPECT_EQ(tree.get_leaves()[4].hash(), NullifierLeaf::zero().hash());
+    EXPECT_EQ(tree.get_leaves()[5].hash(), NullifierLeaf::zero().hash());
+
+    /**
+     * Add new value 50:
+     *
+     *  index     0       1       2       3        4       5       6       7
+     *  ---------------------------------------------------------------------
+     *  val       0       30      10      20       0       0      50       0
+     *  nextIdx   2       6       3       1        0       0       0       0
+     *  nextVal   10      50      20      30       0       0       0       0
+     */
+    tree.update_element(50);
+    EXPECT_EQ(tree.get_leaves().size(), 7);
+    EXPECT_EQ(tree.get_leaves()[0].hash(), NullifierLeaf({ 0, 2, 10 }).hash());
+    EXPECT_EQ(tree.get_leaves()[1].hash(), NullifierLeaf({ 30, 6, 50 }).hash());
+    EXPECT_EQ(tree.get_leaves()[2].hash(), NullifierLeaf({ 10, 3, 20 }).hash());
+    EXPECT_EQ(tree.get_leaves()[3].hash(), NullifierLeaf({ 20, 1, 30 }).hash());
+    EXPECT_EQ(tree.get_leaves()[4].hash(), NullifierLeaf::zero().hash());
+    EXPECT_EQ(tree.get_leaves()[5].hash(), NullifierLeaf::zero().hash());
+    EXPECT_EQ(tree.get_leaves()[6].hash(), NullifierLeaf({ 50, 0, 0 }).hash());
+
+    // Manually compute the node values
+    auto e000 = tree.get_leaves()[0].hash();
+    auto e001 = tree.get_leaves()[1].hash();
+    auto e010 = tree.get_leaves()[2].hash();
+    auto e011 = tree.get_leaves()[3].hash();
+    auto e100 = tree.get_leaves()[4].hash();
+    auto e101 = tree.get_leaves()[5].hash();
+    auto e110 = tree.get_leaves()[6].hash();
+    auto e111 = tree.get_leaves()[7].hash();
+
+    auto e00 = hash_pair_native(e000, e001);
+    auto e01 = hash_pair_native(e010, e011);
+    auto e10 = hash_pair_native(e100, e101);
+    auto e11 = hash_pair_native(e110, e111);
+
+    auto e0 = hash_pair_native(e00, e01);
+    auto e1 = hash_pair_native(e10, e11);
+    auto root = hash_pair_native(e0, e1);
+
+    // Check the hash path at index 2 and 3
+    // Note: This merkle proof would also serve as a non-membership proof of values in (10, 20) and (20, 30)
+    fr_hash_path expected = {
+        std::make_pair(e010, e011),
+        std::make_pair(e00, e01),
+        std::make_pair(e0, e1),
+    };
+    EXPECT_EQ(tree.get_hash_path(2), expected);
+    EXPECT_EQ(tree.get_hash_path(3), expected);
+    EXPECT_EQ(tree.root(), root);
+
+    // Check the hash path at index 6 and 7
+    expected = {
+        std::make_pair(e110, e111),
+        std::make_pair(e10, e11),
+        std::make_pair(e0, e1),
+    };
+    EXPECT_EQ(tree.get_hash_path(6), expected);
+    EXPECT_EQ(tree.get_hash_path(7), expected);
+}
 TEST(crypto_nullifier_tree, test_nullifier_tree)
 {
     // Create a depth-8 indexed merkle tree
