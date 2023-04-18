34141718e1b750f7cc2b52348056227a1ea34746
LHerskind <lasse.herskind@gmail.com>
2023-04-18 12:31:35 +0000

    initial setup

---
+++ sol/bootstrap.sh
@@ -0,0 +1,26 @@
+
+echo "Installing foundry..."
+rm -rf broadcast cache out
+. ./scripts/install_foundry.sh
+forge install --no-commit
+# Ensure libraries are at the correct version
+git submodule update --init --recursive ./lib
+
+echo "Installing barretenberg..."
+git submodule init
+git submodule update
+
+echo "Downloading srs..."
+cd ../cpp/srs_db
+./download_ignition.sh 3
+#./download_ignition_lagrange.sh 12
+cd ../../sol
+
+echo "Building c++ binaries..."
+./scripts/init.sh
+
+echo "Formatting code..."
+forge fmt
+forge build
+
+echo "Targets built, you are good to go!"
