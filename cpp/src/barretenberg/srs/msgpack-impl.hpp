a81f909466d01d548384342744cb246bd81cf157
ludamad <domuradical@gmail.com>
2023-04-19 19:10:42 -0400

    chore: Remove need to return from msgpack method

--- cpp/src/barretenberg/srs/msgpack-impl.hpp
+++ cpp/src/barretenberg/srs/msgpack-impl.hpp
@@ -36,28 +36,26 @@ concept HasMsgPack = requires(T t, DefineMapArchive ar) { t.msgpack(ar); };
 template <typename T>
 concept HasMsgPackFlat = requires(T t, const DefineArchive& ar) { t.msgpack_flat(ar); };
 
-template <HasMsgPack T>
-// TODO constrain with concept for types that have a msgpack_map() method
-struct convert<T> {
+template <HasMsgPack T> struct convert<T> {
     msgpack::object const& operator()(msgpack::object const& o, T& v) const
     {
-        return v.msgpack(DefineMapArchive{}).unpack(o);
+        v.msgpack([&](auto&... args) { DefineMapArchive{}(args...).msgpack_unpack(o); });
+        return o;
     }
 };
 
-template <HasMsgPackFlat T>
-// TODO constrain with concept for types that have a msgpack() method
-struct convert<T> {
+template <HasMsgPackFlat T> struct convert<T> {
     msgpack::object const& operator()(msgpack::object const& o, T& v) const
     {
-        return v.msgpack_flat(DefineArchive{}).msgpack_unpack(o);
+        v.msgpack_flat([&](auto&... args) { DefineArchive{}(args...).msgpack_unpack(o); });
+        return o;
     }
 };
 
 template <HasMsgPack T> struct pack<T> {
     template <typename Stream> packer<Stream>& operator()(msgpack::packer<Stream>& o, T const& v) const
     {
-        const_cast<T&>(v).msgpack(DefineMapArchive{}).msgpack_pack(o);
+        const_cast<T&>(v).msgpack([&](auto&... args) { DefineMapArchive{}(args...).msgpack_pack(o); });
         return o;
     }
 };
@@ -65,21 +63,21 @@ template <HasMsgPack T> struct pack<T> {
 template <HasMsgPackFlat T> struct pack<T> {
     template <typename Stream> packer<Stream>& operator()(msgpack::packer<Stream>& o, T const& v) const
     {
-        const_cast<T&>(v).msgpack_flat(DefineArchive{}).msgpack_pack(o);
+        const_cast<T&>(v).msgpack_flat([&](auto&... args) { DefineArchive{}(args...).msgpack_pack(o); });
         return o;
     }
 };
 
-template <HasMsgPack T> struct object_with_zone<T> {
-    void operator()(msgpack::object::with_zone& o, T const& v) const
-    {
-        const_cast<T&>(v).msgpack(DefineMapArchive{}).msgpack_object(&o, o.zone);
-    }
-};
-template <HasMsgPackFlat T> struct object_with_zone<T> {
-    void operator()(msgpack::object::with_zone& o, T const& v) const
-    {
-        const_cast<T&>(v).msgpack_flat(DefineArchive{}).msgpack_object(o);
-    }
-};
+// template <HasMsgPack T> struct object_with_zone<T> {
+//     void operator()(msgpack::object::with_zone& o, T const& v) const
+//     {
+//         const_cast<T&>(v).msgpack(DefineMapArchive{}).msgpack_object(&o, o.zone);
+//     }
+// };
+// template <HasMsgPackFlat T> struct object_with_zone<T> {
+//     void operator()(msgpack::object::with_zone& o, T const& v) const
+//     {
+//         const_cast<T&>(v).msgpack_flat(DefineArchive{}).msgpack_object(o);
+//     }
+// };
 } // namespace msgpack::adaptor
