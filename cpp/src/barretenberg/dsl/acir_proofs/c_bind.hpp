47bc3e3939d3489f2dca962f9d1695da050963e1
Maxim Vezenov <mvezenov@gmail.com>
2023-04-19 22:21:41 -0400

    c bind functions

--- cpp/src/barretenberg/dsl/acir_proofs/c_bind.hpp
+++ cpp/src/barretenberg/dsl/acir_proofs/c_bind.hpp
@@ -14,6 +14,13 @@ WASM_EXPORT size_t acir_proofs_init_verification_key(void* pippenger,
                                                      uint8_t const* g2x,
                                                      uint8_t const* pk_buf,
                                                      uint8_t const** vk_buf);
+WASM_EXPORT size_t acir_serialize_verification_key_into_field_elements(uint8_t const* g2x,
+                                                                       uint8_t const* vk_buf,
+                                                                       uint8_t** serialized_vk_buf,
+                                                                       uint8_t** serialized_vk_hash_buf);
+WASM_EXPORT size_t acir_serialize_proof_into_field_elements(uint8_t const* proof_data_buf,
+                                                            uint8_t** serialized_proof_data_buf,
+                                                            size_t proof_data_length);
 WASM_EXPORT size_t acir_proofs_new_proof(void* pippenger,
                                          uint8_t const* g2x,
                                          uint8_t const* pk_buf,
