c3d90da211a7961e3b9ab9443739e6abbc2e1ebf
ludamad <domuradical@gmail.com>
2023-04-28 16:47:45 -0400

    fix: remove shared ptr reference

--- cpp/src/barretenberg/common/msgpack/concepts.hpp
+++ cpp/src/barretenberg/common/msgpack/concepts.hpp
@@ -8,6 +8,4 @@ template<typename T>
 concept HasMsgPack = requires(T t, DoNothing nop) { t.msgpack(nop); };
 template<typename T>
 concept HasMsgPackPack = requires(T t, DoNothing nop) { t.msgpack_pack(nop); };
-template<typename T>
-concept SharedPtr = std::is_same_v<T, std::shared_ptr<typename T::element_type>>;
 }
