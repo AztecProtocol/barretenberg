37122e7a519c46bb272d2187721d311614fc0c46
ludamad <domuradical@gmail.com>
2023-05-01 10:25:36 -0400

    Msgpack test fix

--- cpp/src/barretenberg/ecc/fields/.test.cpp
+++ cpp/src/barretenberg/ecc/fields/.test.cpp
@@ -1,13 +1,14 @@
 #include "field.hpp"
 #include <gtest/gtest.h>
 #include "barretenberg/common/msgpack_impl.hpp"
+#include "barretenberg/ecc/curves/bn254/fr.hpp"
 
 TEST(field_msgpack, basic)
 {
-    barretenberg::field field(1ull, 2ull, 3ull, 4ull);
-    barretenberg::field result(0, 0, 0, 0);
+    barretenberg::fr field {1ull, 2ull, 3ull, 4ull};
+    barretenberg::fr result {0, 0, 0, 0};
     msgpack::sbuffer buffer;
     msgpack::pack(buffer, field);
     msgpack::unpack(buffer.data(), buffer.size()).get().convert(result);
-    EXPECT_EQ(result, expected);
+    EXPECT_EQ(field, result);
 }
