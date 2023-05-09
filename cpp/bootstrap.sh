22fc7e9c70dbf43f0d5d5129319222977b78999d
Charlie Lye <karl.lye@gmail.com>
2023-05-09 14:42:03 +0000

    WASI stuff.

--- cpp/bootstrap.sh
+++ cpp/bootstrap.sh
@@ -64,9 +64,20 @@ cmake --preset $PRESET -DCMAKE_BUILD_TYPE=RelWithAssert
 cmake --build --preset $PRESET ${@/#/--target }
 
 # Install the webassembly toolchain.
-WASI_VERSION=20
 cd ./src
-curl -s -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$WASI_VERSION/wasi-sdk-$WASI_VERSION.0-$OS.tar.gz | tar zxfv -
+curl -s -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-20/wasi-sdk-20.0-$OS.tar.gz | tar zxfv -
+if [ "$OS" == "linux" ]; then
+  # Exceptional linux case that provides an llvm build that works on Ubuntu 20.
+  curl -s -L https://wasi-sdk.s3.eu-west-2.amazonaws.com/yamt-wasi-sdk-20.0.threads.tgz | tar zxfv -
+else
+  # For other operating systems, first download the standard release (this is to get the llvm build).
+  curl -s -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-20.threads/wasi-sdk-20.0+threads-$OS.tar.gz | tar zxfv -
+  # Replace wasi-sysroot in wasi-sdk-20.0+threads with our custom build.
+  # It contains libc++ and a patch by yamt to improve thread join stability.
+  # Can remove once future releases are more stable.
+  curl -s -L curl -O -L https://wasi-sdk.s3.eu-west-2.amazonaws.com/yamt-wasi-sysroot-20.0.threads.tgz | tar zxfv -
+fi
+tar zxfv wasi-sysroot-20.0+threads.yamt.tgz
 cd ..
 
 # Build WASM.
