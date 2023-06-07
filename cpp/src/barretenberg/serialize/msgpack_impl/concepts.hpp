d1e7a796e404466cdbfdad7dc922dcff0b81feaf
ludamad <adam.domurad@gmail.com>
2023-06-07 10:24:27 -0400

    Update concepts.hpp

--- cpp/src/barretenberg/serialize/msgpack_impl/concepts.hpp
+++ cpp/src/barretenberg/serialize/msgpack_impl/concepts.hpp
@@ -6,8 +6,10 @@ struct DoNothing {
 namespace msgpack_concepts {
 template <typename T>
 concept HasMsgPack = requires(T t, DoNothing nop) { t.msgpack(nop); };
+
 template <typename T>
 concept HasMsgPackSchema = requires(const T t, DoNothing nop) { t.msgpack_schema(nop); };
+
 template <typename T>
 concept HasMsgPackPack = requires(T t, DoNothing nop) { t.msgpack_pack(nop); };
 } // namespace msgpack_concepts
