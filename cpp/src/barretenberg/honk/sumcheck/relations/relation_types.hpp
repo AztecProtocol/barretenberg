17ace62cd9ba49753aa5e59c563be6e5c041da2c
ledwards2225 <l.edwards.d@gmail.com>
2023-06-07 19:42:19 +0000

    adding a comment to explain the purpose of rlation types

--- cpp/src/barretenberg/honk/sumcheck/relations/relation_types.hpp
+++ cpp/src/barretenberg/honk/sumcheck/relations/relation_types.hpp
@@ -7,6 +7,21 @@
 
 namespace proof_system::honk::sumcheck {
 
+/**
+ * @brief The templates defined herein facilitate sharing the relation arithmetic between the prover and the verifier.
+ *
+ * The sumcheck prover and verifier accumulate the contributions from each relation (really, each sub-relation) into,
+ * respectively, Univariates and individual field elements. When performing relation arithmetic on Univariates, we
+ * introduce UnivariateViews to reduce full length Univariates to the minimum required length and to avoid unnecessary
+ * copies.
+ *
+ * To share the relation arithmetic, we introduce simple structs that specify two types: Accumulators and
+ * AccumulatorViews. For the prover, who accumulates Univariates, these are respectively std::tuple<Univariate> and
+ * std::tuple<UnivariateView>. For the verifier, who accumulates FFs, both types are simply aliases for std::array<FF>
+ * (since no "view" type is necessary). The containers std::tuple and std::array are needed to accommodate multiple
+ * sub-relations within each relation, where, for efficiency, each sub-relation has its own specified degree.
+ */
+
 // Helper struct to allow passing an arbitrary collection of lengths to the AccumulatorTypes
 template <size_t... Values> struct LengthsWrapper {};
 
@@ -17,7 +32,7 @@ template <typename FF, typename LengthsWrapper> struct ValueAccumulatorTypesHelp
 // Helper to define value (FF) based accumulator types
 template <typename FF, size_t... Values> struct ValueAccumulatorTypesHelper<FF, LengthsWrapper<Values...>> {
     using Accumulators = std::array<FF, sizeof...(Values)>;
-    using AccumulatorViews = std::array<FF, sizeof...(Values)>;
+    using AccumulatorViews = std::array<FF, sizeof...(Values)>; // there is no "view" type here
 };
 
 // Accumulator types for values (FFs)
