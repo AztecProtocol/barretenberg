d622c13495274502321716234d7193caaef2b781
github-action-benchmark <github@users.noreply.github.com>
2023-06-09 17:04:50 +0000

    add C++ Benchmark (googlecpp) benchmark result for 136f9a6148579897400fb6265032971105ac7a4c

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1686330290666,
+  "repoUrl": "https://github.com/AztecProtocol/barretenberg",
+  "entries": {
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "committer": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "id": "136f9a6148579897400fb6265032971105ac7a4c",
+          "message": "Benchmark suite update",
+          "timestamp": "2023-06-06T14:27:00Z",
+          "url": "https://github.com/AztecProtocol/barretenberg/pull/508/commits/136f9a6148579897400fb6265032971105ac7a4c"
+        },
+        "date": 1686330289397,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "construct_proof/sha256/10/repeats:1",
+            "value": 15351686012.999949,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 13038763200 ns\nthreads: 1"
+          },
+          {
+            "name": "construct_proof/keccak/10/repeats:1",
+            "value": 25777982156.999996,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 23422014300 ns\nthreads: 1"
+          },
+          {
+            "name": "construct_proof/ecdsa_verification/10/repeats:1",
+            "value": 47603325762.00005,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 44985187800.000015 ns\nthreads: 1"
+          },
+          {
+            "name": "construct_proof/merkle_membership/10/repeats:1",
+            "value": 4927383514.999861,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 3037812700.0000033 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
