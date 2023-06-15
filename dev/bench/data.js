840347fe309d89d29b8bf0136d30c0a1e3e5351b
github-action-benchmark <github@users.noreply.github.com>
2023-06-15 18:15:20 +0000

    add C++ Benchmark (googlecpp) benchmark result for b20b4012546c5b67623950d0fedb0974df8bf345

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686830727631,
+  "lastUpdate": 1686852919992,
   "repoUrl": "https://github.com/AztecProtocol/barretenberg",
   "entries": {
     "C++ Benchmark": [
@@ -96,6 +96,54 @@ window.BENCHMARK_DATA = {
             "value": 4.592230771000118,
             "unit": "s/iter",
             "extra": "iterations: 1\ncpu: 2.6973589000000118 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "isennovskiy@gmail.com",
+            "name": "Innokentii Sennovskii",
+            "username": "Rumata888"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b20b4012546c5b67623950d0fedb0974df8bf345",
+          "message": "feat(ultrahonk): Added a simple filler table to minimize the amount of entries used to make UltraHonk polynomials non-zero (#531)\n\nfeat(ultrahonk): Added a simple filler table to minimize the amount of entries used to make UltraHonk polynomials non-zero",
+          "timestamp": "2023-06-15T19:05:12+01:00",
+          "tree_id": "e57ef15fc06d2a705d20fbe22470e35ac4d1fc3e",
+          "url": "https://github.com/AztecProtocol/barretenberg/commit/b20b4012546c5b67623950d0fedb0974df8bf345"
+        },
+        "date": 1686852919127,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "construct_proof_ultra/sha256/10/repeats:1",
+            "value": 7.3394697190000215,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 5.7652163 s\nthreads: 1"
+          },
+          {
+            "name": "construct_proof_ultra/keccak/10/repeats:1",
+            "value": 19.427090368999984,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 16.8724049 s\nthreads: 1"
+          },
+          {
+            "name": "construct_proof_ultra/ecdsa_verification/10/repeats:1",
+            "value": 35.15534873800004,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 32.633811 s\nthreads: 1"
+          },
+          {
+            "name": "construct_proof_ultra/merkle_membership/10/repeats:1",
+            "value": 4.367971393000062,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 2.382261900000003 s\nthreads: 1"
           }
         ]
       }
