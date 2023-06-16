ab016eb3e6f6e78971934a36cb3379386201a91a
jonybur <jobur93@gmail.com>
2023-06-16 22:26:07 +0000

    Switch point to be browser compatible

--- ts/src/types/point.ts
+++ ts/src/types/point.ts
@@ -1,3 +1,5 @@
+// TODO: fix this file for browser (already did)
+
 import { Fr } from './index.js';
 import { BufferReader } from '../serialize/buffer_reader.js';
 
@@ -18,15 +20,30 @@ export class Point {
   }
 
   static fromString(address: string) {
-    return Point.fromBuffer(Buffer.from(address.replace(/^0x/i, ''), 'hex'));
+    address = address.replace(/^0x/i, '');
+    const byteValues = new Uint8Array(Math.ceil(address.length / 2));
+    for (let i = 0; i < byteValues.length; i++) {
+      byteValues[i] = Number.parseInt(address.substr(i * 2, 2), 16);
+    }
+    return Point.fromBuffer(byteValues);
   }
 
   toBuffer() {
-    return Buffer.concat([this.x.toBuffer(), this.y.toBuffer()]);
+    const xBuffer = this.x.toBuffer();
+    const yBuffer = this.y.toBuffer();
+    const combined = new Uint8Array(xBuffer.length + yBuffer.length);
+    combined.set(xBuffer, 0);
+    combined.set(yBuffer, xBuffer.length);
+    return combined;
   }
 
   toString() {
-    return '0x' + this.toBuffer().toString('hex');
+    const buffer = this.toBuffer();
+    let hexString = '0x';
+    for (let i = 0; i < buffer.length; i++) {
+      hexString += buffer[i].toString(16).padStart(2, '0');
+    }
+    return hexString;
   }
 
   equals(rhs: Point) {
