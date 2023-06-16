665a54acf1c18d71e63aeb60a194a8b5d70d81be
codygunton <codygunton@gmail.com>
2023-06-16 16:54:45 +0000

    Revert "Fix bb.js from Charlie"
    This reverts commit c0503b8329f445e83487a23cb6d55049733d454f.

--- cpp/src/barretenberg/proof_system/polynomial_store/polynomial_store_cache.hpp
+++ cpp/src/barretenberg/proof_system/polynomial_store/polynomial_store_cache.hpp
@@ -10,8 +10,8 @@ namespace proof_system {
  * A cache that wraps an underlying external store. It favours holding the largest polynomials in it's cache up
  * to max_cache_size_ polynomials. This saves on many expensive copies of large amounts of memory to the external
  * store. Smaller polynomials get swapped out, but they're also much cheaper to read/write.
- * The default ctor sets the cache size to 40.
- * In combination with the slab allocator, this brings us to about 3.8GB mem usage for 512k circuits.
+ * The default ctor sets the cache size to 70.
+ * In combination with the slab allocator, this brings us to about 4GB mem usage for 512k circuits.
  * In tests using just the external store increased proof time from by about 50%.
  * This pretty much recoups all losses.
  */
