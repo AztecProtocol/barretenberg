dd0cddb5eb6fe90f84dbc21b17c31dece9e247ff
jonybur <jobur93@gmail.com>
2023-06-21 10:59:30 +0000

    Revert node files

--- ts/src/barretenberg_wasm/node/worker.ts
+++ ts/src/barretenberg_wasm/node/worker.ts
@@ -1,10 +1,10 @@
-import { parentPort } from 'worker_threads';	
-import { expose } from 'comlink';	
-import { BarretenbergWasm } from '../index.js';	
-import { nodeEndpoint } from './node_endpoint.js';	
+import { parentPort } from 'worker_threads';
+import { expose } from 'comlink';
+import { BarretenbergWasm } from '../index.js';
+import { nodeEndpoint } from './node_endpoint.js';
 
-if (!parentPort) {	
-  throw new Error('No parentPort');	
-}	
+if (!parentPort) {
+  throw new Error('No parentPort');
+}
 
-expose(new BarretenbergWasm(), nodeEndpoint(parentPort));	
+expose(new BarretenbergWasm(), nodeEndpoint(parentPort));
