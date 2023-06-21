9a2131afab287e9579984981e11a71961b1482fa
jonybur <jobur93@gmail.com>
2023-06-21 14:10:40 +0000

    Adds point for node and browser

--- ts/src/types/index.ts
+++ ts/src/types/index.ts
@@ -1,5 +1,5 @@
 export * from './ptr.js';
 export * from './fields.js';
-export * from './point.js';
+export * from './node/index.js';
 export * from './fixed_size_buffer.js';
 export * from './raw_buffer.js';
