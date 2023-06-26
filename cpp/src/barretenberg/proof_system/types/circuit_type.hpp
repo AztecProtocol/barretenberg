503086d38c6e884965346fd2a6ad5e82b181ad79
codygunton <codygunton@gmail.com>
2023-06-26 03:42:12 +0000

    FIx j-s test.

--- cpp/src/barretenberg/proof_system/types/circuit_type.hpp
+++ cpp/src/barretenberg/proof_system/types/circuit_type.hpp
@@ -3,7 +3,7 @@
 #include <concepts>
 
 namespace proof_system {
-enum class CircuitType : uint32_t { UNDEFINED, STANDARD, TURBO, ULTRA };
+enum class CircuitType : uint32_t { STANDARD, TURBO, ULTRA, UNDEFINED };
 
 template <typename T, typename... U> concept IsAnyOf = (std::same_as<T, U> || ...);
 } // namespace proof_system
