02178b2355f8ffbd6a48838b194de8df97378a88
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-06-29 15:17:47 +0000

    fix: biggroup curves with a bug

--- cpp/src/barretenberg/stdlib/encryption/ecdsa/ecdsa_impl.hpp
+++ cpp/src/barretenberg/stdlib/encryption/ecdsa/ecdsa_impl.hpp
@@ -165,6 +165,7 @@ bool_t<Composer> verify_signature_prehashed_message_noassert(const stdlib::byte_
     if constexpr (HasPlookup<Composer> && Curve::type == proof_system::CurveType::SECP256K1) {
         result = G1::secp256k1_ecdsa_mul(public_key, u1, u2);
     } else {
+        public_key.validate_on_curve();
         result = G1::batch_mul({ G1::one(ctx), public_key }, { u1, u2 });
     }
     result.x.self_reduce();
