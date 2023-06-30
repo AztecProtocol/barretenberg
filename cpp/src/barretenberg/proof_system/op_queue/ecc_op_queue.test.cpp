be288b3b18c1f324e89c127318031436abb00583
codygunton <codygunton@gmail.com>
2023-06-30 16:00:17 +0000

    Add new ops, change curve, make const, document.

--- cpp/src/barretenberg/proof_system/op_queue/ecc_op_queue.test.cpp
+++ cpp/src/barretenberg/proof_system/op_queue/ecc_op_queue.test.cpp
@@ -5,10 +5,10 @@ namespace proof_system::test_flavor {
 TEST(ECCOpQueueTest, Basic)
 {
     ECCOpQueue op_queue;
-    op_queue.add_accumulate(grumpkin::g1::affine_one);
-    EXPECT_EQ(op_queue._data[0].base_point, grumpkin::g1::affine_one);
+    op_queue.add_accumulate(barretenberg::g1::affine_one);
+    EXPECT_EQ(op_queue.raw_ops[0].base_point, barretenberg::g1::affine_one);
     op_queue.empty_row();
-    EXPECT_EQ(op_queue._data[1].add, false);
+    EXPECT_EQ(op_queue.raw_ops[1].add, false);
 }
 
 } // namespace proof_system::test_flavor
