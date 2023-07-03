0e5b1d34bd4fdc9a65405ad300c4e8ee09eaaa07
codygunton <codygunton@gmail.com>
2023-07-03 08:19:47 +0000

    Fully sort unreduced graph rep.

--- cpp/scripts/get_bb_module_graph.sh
+++ cpp/scripts/get_bb_module_graph.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 set -eu
 
-# find ../src/barretenberg -type f -iname "CMakeLists.txt" -exec grep "barretenberg_module" {} + | awk -F " " '{print $2}' >> bb_modules.graph
 find ../src/barretenberg -type f -iname "CMakeLists.txt" -exec grep "barretenberg_module" {} + > bb_modules
 
 python << END
@@ -10,18 +9,19 @@ import re
 
 with open('bb_modules_unreduced.graph', 'w') as outfile:
     outfile.write("digraph G {\n")
-    sorted_lines = []
+    lines = []
     with open('bb_modules', 'r') as infile:
         for line in infile:
             line = re.findall(r'barretenberg_module\(([^]]*)\)', line)[0]
             line = line.replace("-", "_")
-            sorted_lines.append(line)
+            lines.append(line)
 
-        sorted_lines.sort()
-        for line in sorted_lines:
+        lines.sort()
+        for line in lines:
             deps = line.split(" ")
             module = deps[0]
             deps = deps[1:]
+            deps.sort()
             for dep in deps:
                 outfile.write("        " + module + " -> " + dep + ";\n")
     outfile.write("}\n")
@@ -45,3 +45,5 @@ with open('bb_modules_reduced.graph', 'w') as outfile:
 END
 
 rm bb_modules_reduced_not_sorted.graph
+
+diff -c bb_modules_unreduced.graph bb_modules_reduced.graph
