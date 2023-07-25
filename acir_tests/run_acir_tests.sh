8da081d746c6ebce1dde5851e2acd3e4a5a743a3
kevaundray <kevtheappdev@gmail.com>
2023-07-25 20:53:15 +0000

    use branch

--- acir_tests/run_acir_tests.sh
+++ acir_tests/run_acir_tests.sh
@@ -7,7 +7,7 @@ set -e
 
 BB=$PWD/${BB:-../cpp/build/bin/bb}
 CRS_PATH=~/.bb-crs
-BRANCH=master
+BRANCH=kw/update-acir-tests
 
 # Pull down the test vectors from the noir repo, if we don't have the folder already.
 if [ ! -d acir_tests ]; then
