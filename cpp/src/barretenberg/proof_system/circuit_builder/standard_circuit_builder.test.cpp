c8dc7de41768970371ca7088fe6cf0868af7f379
Sarkoxed <sarkoxed2013@yandex.ru>
2023-07-27 15:06:50 +0000

    polynomial evaluation tests

--- cpp/src/barretenberg/proof_system/circuit_builder/standard_circuit_builder.test.cpp
+++ cpp/src/barretenberg/proof_system/circuit_builder/standard_circuit_builder.test.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <string>
 
+#include "barretenberg/stdlib/primitives/field/field.hpp"
+
 using namespace barretenberg;
 using namespace proof_system;
 
@@ -505,4 +507,89 @@ TEST(standard, functionality_check)
     builder.export_circuit(myfile);
     builder.export_circuit(std::cout);
 }
+
+TEST(standard, test_circuit)
+{
+    StandardCircuitBuilder builder = StandardCircuitBuilder();
+    size_t n = 3;
+    std::vector<uint32_t> coeffs = { 17, 20, 1, 10, 1, 12, 18, 6, 0, 4, 2, 14, 9, 19, 16, 11, 2, 13, 18, 6 };
+    std::vector<uint32_t> idxs;
+    for (size_t i = 0; i < n; i++) {
+        uint32_t idx = builder.add_public_variable(coeffs[i]);
+        idxs.push_back(idx);
+        builder.set_variable_name(idx, "coeff_" + std::to_string(i));
+    }
+
+    fr z(10);
+    uint32_t z_idx = builder.add_variable(z);
+    builder.set_variable_name(z_idx, "point");
+    fr res = fr::zero();
+    uint32_t res_idx = builder.zero_idx; // i think assert_equal was needed for zero initialization
+    builder.assert_equal(res_idx, 0);
+
+    for (size_t i = 0; i < n; i++) {
+        res = res * z;
+        uint32_t mul_idx = builder.add_variable(res);
+        // builder.set_variable_name(mul_idx, "mul_" + std::to_string(i));
+        builder.create_mul_gate({ res_idx, z_idx, mul_idx, fr::one(), fr::neg_one(), fr::zero() });
+
+        res = res + coeffs[i];
+        uint32_t add_idx = builder.add_variable(res);
+        builder.create_add_gate({
+            mul_idx,
+            idxs[i],
+            add_idx,
+            fr::one(),
+            fr::one(),
+            fr::neg_one(),
+            fr::zero(),
+        });
+
+        res_idx = add_idx;
+    }
+    builder.set_variable_name(res_idx, "result");
+    EXPECT_TRUE(builder.check_circuit());
+    info(res);
+    info(builder.num_gates);
+    info(builder.get_num_variables());
+    info(builder.get_num_public_inputs());
+
+    std::ofstream myfile;
+    myfile.open("strict.json", std::ios::out | std::ios::trunc | std::ios::binary);
+
+    builder.export_circuit(myfile);
+}
+
+TEST(standard, test_circuit_field) // TODO check how to add variable names here
+{
+    using field_ct = proof_system::plonk::stdlib::field_t<StandardCircuitBuilder>;
+    using witness_t = proof_system::plonk::stdlib::witness_t<StandardCircuitBuilder>;
+    using pub_witness_t = proof_system::plonk::stdlib::public_witness_t<StandardCircuitBuilder>;
+
+    StandardCircuitBuilder builder = StandardCircuitBuilder();
+
+    size_t n = 20;
+    std::vector<uint32_t> cfs = { 17, 20, 1, 10, 1, 12, 18, 6, 0, 4, 2, 14, 9, 19, 16, 11, 2, 13, 18, 6 };
+    std::vector<field_ct> coeffs;
+    for (size_t i = 0; i < n; i++) {
+        coeffs.emplace_back(field_ct(pub_witness_t(&builder, cfs[i])));
+    }
+    field_ct z(witness_t(&builder, 10));
+    field_ct res(witness_t(&builder, fr::zero()));
+
+    for (size_t i = 0; i < n; i++) {
+        res = res * z + coeffs[i];
+    }
+    EXPECT_TRUE(builder.check_circuit());
+    info(res);
+    info(builder.num_gates);
+    info(builder.get_num_variables());
+    info(builder.get_num_public_inputs());
+
+    std::ofstream myfile;
+    myfile.open("field.json", std::ios::out | std::ios::trunc | std::ios::binary);
+
+    builder.export_circuit(myfile);
+}
+
 } // namespace standard_circuit_constructor_tests
