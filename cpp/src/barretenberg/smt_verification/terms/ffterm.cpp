17cd968525518ea132025c01a64dd820005156bb
Sarkoxed <sarkoxed2013@yandex.ru>
2023-08-29 10:41:53 +0000

    Bool and FFterm separation

--- cpp/src/barretenberg/smt_verification/terms/ffterm.cpp
+++ cpp/src/barretenberg/smt_verification/terms/ffterm.cpp
@@ -92,40 +92,4 @@ void FFTerm::operator!=(const FFTerm& other) const
     eq = this->solver->s.mkTerm(cvc5::Kind::EQUAL, { eq, this->solver->s.mkBoolean(false) });
     this->solver->s.assertFormula(eq);
 }
-
-Bool Bool::operator|(const Bool& other) const
-{
-    cvc5::Term res = solver->mkTerm(cvc5::Kind::OR, { this->term, other.term });
-    ;
-    return { res, this->solver };
-}
-
-void Bool::operator|=(const Bool& other)
-{
-    this->term = this->solver->mkTerm(cvc5::Kind::OR, { this->term, other.term });
-}
-
-Bool Bool::operator&(const Bool& other) const
-{
-    cvc5::Term res = solver->mkTerm(cvc5::Kind::AND, { this->term, other.term });
-    return { res, this->solver };
-}
-
-void Bool::operator&=(const Bool& other)
-{
-    this->term = this->solver->mkTerm(cvc5::Kind::AND, { this->term, other.term });
-}
-
-Bool Bool::operator==(const Bool& other) const
-{
-    cvc5::Term res = solver->mkTerm(cvc5::Kind::EQUAL, { this->term, other.term });
-    return { res, this->solver };
-}
-
-Bool Bool::operator!=(const Bool& other) const
-{
-    cvc5::Term res = solver->mkTerm(cvc5::Kind::EQUAL, { this->term, other.term });
-    res = solver->mkTerm(cvc5::Kind::EQUAL, { res, this->solver->mkBoolean(false) });
-    return { res, this->solver };
-}
-}; // namespace smt_terms
+} // namespace smt_terms
--- cpp/src/barretenberg/smt_verification/terms/ffterm.hpp
+++ cpp/src/barretenberg/smt_verification/terms/ffterm.hpp
@@ -4,16 +4,12 @@
 namespace smt_terms {
 using namespace smt_solver;
 
-class FFTerm;
-class Bool;
-
 class FFTerm {
-  private:
+  public:
     Solver* solver;
     cvc5::Term term;
     bool isconst;
 
-  public:
     FFTerm()
         : solver(nullptr)
         , term(cvc5::Term())
@@ -66,81 +62,9 @@ class FFTerm {
         cvc5::Term res = slv->s.mkTerm(cvc5::Kind::FINITE_FIELD_MULT, terms);
         return { res, slv };
     }
-
-    friend class Bool;
 };
 
 FFTerm Var(const std::string& name, Solver* slv);
 FFTerm Const(const std::string& val, Solver* slv, uint32_t base = 16);
 
-class Bool { // TODO(alex) separate them into two files idkh
-  private:
-    cvc5::Solver* solver;
-    cvc5::Term term;
-    bool asserted = false;
-
-  public:
-    explicit Bool(const cvc5::Term& t, Solver& slv)
-        : solver(&slv.s)
-        , term(t){};
-    explicit Bool(const FFTerm& t)
-        : solver(&t.solver->s)
-        , term(t.term){};
-
-    explicit Bool(bool t, Solver& slv)
-        : solver(&slv.s)
-    {
-        term = solver->mkBoolean(t);
-    }
-    Bool(const cvc5::Term& term, cvc5::Solver* s)
-        : solver(s)
-        , term(term){};
-    Bool(const Bool& other) = default;
-    Bool(Bool&& other) = default;
-
-    Bool& operator=(const Bool& right) = default;
-    Bool& operator=(Bool&& right) = default;
-
-    void assert_term()
-    {
-        if (!asserted) {
-            solver->assertFormula(term);
-            asserted = true;
-        }
-    }
-
-    Bool operator|(const Bool& other) const;
-    void operator|=(const Bool& other);
-    Bool operator|(const bool& other) const;
-    void operator|=(const bool& other) const;
-
-    Bool operator&(const Bool& other) const;
-    void operator&=(const Bool& other);
-    Bool operator&(const bool& other) const;
-    void operator&=(const bool& other);
-
-    Bool operator==(const Bool& other) const;
-    Bool operator!=(const Bool& other) const;
-
-    operator std::string() const { return term.toString(); };
-    operator cvc5::Term() const { return term; };
-
-    friend Bool batch_or(const std::vector<Bool>& children)
-    {
-        cvc5::Solver* s = children[0].solver;
-        std::vector<cvc5::Term> terms(children.begin(), children.end());
-        cvc5::Term res = s->mkTerm(cvc5::Kind::OR, terms);
-        return { res, s };
-    }
-
-    friend Bool batch_and(const std::vector<Bool>& children)
-    {
-        cvc5::Solver* s = children[0].solver;
-        std::vector<cvc5::Term> terms(children.begin(), children.end());
-        cvc5::Term res = s->mkTerm(cvc5::Kind::AND, terms);
-        return { res, s };
-    }
-
-    ~Bool() = default;
-};
-}; // namespace smt_terms
+} // namespace smt_terms
