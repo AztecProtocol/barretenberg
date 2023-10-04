f89772a531702d03b18ed43516ae40a0a26b0d1c
spypsy <spypsy@users.noreply.github.com>
2023-10-04 13:01:27 +0100

    fix: include ignition data in package or save after 1st download (#2591)
    Fixes #2445

--- cpp/srs_db/download_ignition.sh
+++ cpp/srs_db/download_ignition.sh
@@ -20,6 +20,9 @@ cd ignition
 mkdir -p monomial
 cd monomial
 NUM=${1:-19}
+RANGE_START=${2:-}
+RANGE_END=${3:-}
+APPEND=${4:-"false"}
 
 if command -v sha256sum > /dev/null; then
   SHASUM=sha256sum
@@ -33,12 +36,26 @@ checksum() {
 }
 
 download() {
-  curl https://aztec-ignition.s3-eu-west-2.amazonaws.com/MAIN%20IGNITION/monomial/transcript${1}.dat > transcript${1}.dat
+  # Initialize an empty variable for the Range header
+  RANGE_HEADER=""
+  
+  # If both RANGE_START and RANGE_END are set, add them to the Range header
+  if [ -n "$RANGE_START" ] && [ -n "$RANGE_END" ]; then
+    RANGE_HEADER="-H Range:bytes=$RANGE_START-$RANGE_END"
+  fi
+  
+  # Download the file
+  if [ "$APPEND" = "true" ]; then
+    curl $RANGE_HEADER https://aztec-ignition.s3-eu-west-2.amazonaws.com/MAIN%20IGNITION/monomial/transcript${1}.dat >> transcript${1}.dat
+  else
+    curl $RANGE_HEADER https://aztec-ignition.s3-eu-west-2.amazonaws.com/MAIN%20IGNITION/monomial/transcript${1}.dat > transcript${1}.dat
+  fi
+  
 }
 
 for TRANSCRIPT in $(seq 0 $NUM); do
   NUM=$(printf %02d $TRANSCRIPT)
-  if [ -f checksums ]; then
+  if [ -f checksums  ] && [ -z "$RANGE_START" ] && [ -z "$RANGE_END" ] ; then
     checksum $NUM && continue
     download $NUM
     checksum $NUM || exit 1
